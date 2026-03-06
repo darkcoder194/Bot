f786d79c91b3f1858624abcf2221cf64b75ade93
darkcoder194 <darkcoder194@gmail.com>
2026-03-06 10:39:28 +0530

    Update hashtag_generator.py

---
+++ hashtag_generator.py
@@ -0,0 +1,17 @@
+import random
+
+tags = [
+
+"#anime",
+"#otaku",
+"#manga",
+"#animeupdate",
+"#animeworld",
+"#weeb",
+"#animelover",
+"#animeedit"
+]
+
+def generate():
+
+    return " ".join(random.sample(tags,5))
